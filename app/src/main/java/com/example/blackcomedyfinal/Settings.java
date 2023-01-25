5775672eae97445e6052a304e8febe91040458b5
DanielAlmeida2003 <danielalmeida.da3@gmail.com>
2023-01-25 23:24:45 +0000

    Upload Imagem

--- app/src/main/java/com/example/blackcomedyfinal/Settings.java
+++ app/src/main/java/com/example/blackcomedyfinal/Settings.java
@@ -1,14 +1,19 @@
 package com.example.blackcomedyfinal;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,12 +24,21 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.blackcomedyfinal.models.Utilizadores;
 import com.example.blackcomedyfinal.singleton.singleuser;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Settings extends AppCompatActivity {
 
@@ -32,6 +46,7 @@ public class Settings extends AppCompatActivity {
     ImageView profileImage;
     public static final int PICK_IMAGE = 1;
     EditText nome, password , email,bio;
+    Uri downloadUrl;
 
     Uri filePath;
 
@@ -85,9 +100,6 @@ public class Settings extends AppCompatActivity {
                 in.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(in, "Select Picture"),PICK_IMAGE);
 
-
-
-                //FirebaseFirestore db = FirebaseFirestore.getInstance();
             }
         });
 
@@ -113,20 +125,85 @@ public class Settings extends AppCompatActivity {
                 }else{
 
                     FirebaseFirestore db = FirebaseFirestore.getInstance();
-                    Map<String, Object> updates = new HashMap<>();
 
-                    updates.put("Nome", nome.getText().toString());
-                    updates.put("Password", password.getText().toString());
-                    updates.put("Email", email.getText().toString());
-                    updates.put("Bio", bio.getText().toString());
+                    //Firebase Storage
+                    FirebaseStorage storage = FirebaseStorage.getInstance();
+                    StorageReference storageRef = storage.getReference();
+                    StorageReference imagesRef = storageRef.child("profiles");
 
-                    db.collection("users").document(utilizador.getId()).update(updates);
-                    singleuser.utilizador.get(0).setNome(nome.getText().toString());
-                    singleuser.utilizador.get(0).setEmail(email.getText().toString());
-                    singleuser.utilizador.get(0).setPassword(password.getText().toString());
-                    singleuser.utilizador.get(0).setBio(bio.getText().toString());
+                    //Get the image from the ImageView and convert it to a byte array:
+                    BitmapDrawable bitmapDrawable = (BitmapDrawable) profileImage.getDrawable();
+                    Bitmap bitmap = bitmapDrawable.getBitmap();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    byte[] data = baos.toByteArray();
 
-                    Toast.makeText(Settings.this, "Atualizados com sucesso", Toast.LENGTH_SHORT).show();
+                    Date currentTime = Calendar.getInstance().getTime();
+
+                    String newName = nome.getText().toString()  + "-" + "-" + currentTime.toString() +  "profile.png";
+
+                    //Create a new StorageReference for the image you want to upload and upload the image as a byte array:
+                    StorageReference imageRef = imagesRef.child(newName);
+                    UploadTask uploadTask = imageRef.putBytes(data);
+
+                    uploadTask.addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            // ...
+                        }
+                    }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+                            downloadUrl = taskSnapshot.getUploadSessionUri();
+                            StorageReference imageRef = storage.getReference("profiles/" + newName);
+
+                            //Obtenha a URL da imagem:
+                            imageRef.getDownloadUrl()
+                            .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+
+                                    Map<String, Object> updates = new HashMap<>();
+                                    String urlImage = uri.toString();
+
+                                    updates.put("Nome", nome.getText().toString());
+                                    updates.put("Password", password.getText().toString());
+                                    updates.put("Email", email.getText().toString());
+                                    updates.put("Bio", bio.getText().toString());
+
+                                    updates.put("ImagemDePerfil", urlImage);
+
+                                    Toast.makeText(Settings.this, "Atualizados com sucesso", Toast.LENGTH_SHORT).show();
+                                    db.collection("users").document(utilizador.getId()).update(updates);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding Imagem", e);
+                                }
+                            });
+
+
+                            //Toast.makeText(Settings.this, downloadUrl.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+
+                    //;
+
+
+                    //singleuser.utilizador.get(0).setNome(nome.getText().toString());
+                    //singleuser.utilizador.get(0).setEmail(email.getText().toString());
+                    //singleuser.utilizador.get(0).setPassword(password.getText().toString());
+                    //singleuser.utilizador.get(0).setBio(bio.getText().toString());
+
+                    //singleuser.utilizador.get(0).setImage();
+
+
+
 
                 }
 
